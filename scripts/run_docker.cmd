dd7384228d48c8ff9a0d0c334b86994acc9f4cde
Oded <oded.nissan@gmail.com>
2017-09-08 13:03:37 +0300

    cmd file

---
+++ scripts/run_docker.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+echo "Creating image"
+
+
+if "%1"=="" echo "illegal number of parameters" && exit 
+
+set BUILD_NUM=%1
+echo "Running with BUILD_NUM=" %BUILD_NUM%
+
+docker build -t odedns/pet:%BUILD_NUM% .
+
+docker run -d --name t1  --rm -p 8888:8080 odedns/pet
+
+
+
